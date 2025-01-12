082feecb34f517c6f0e6bac441115345ab316d2c
Voslar Matej <msamyel@gmail.com>
2025-01-12 13:38:46 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import InputArea from "./components/InputArea";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <InputArea />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import InputArea from "../components/InputArea";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <div>Home</div>
+            <InputArea />
+        </>
+    );
+};
+
+export default Home;
